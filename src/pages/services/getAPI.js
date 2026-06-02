1d55572dae7305d26aa62962e0c18e477dc0cda0
Fire Laptop <kirellia719@gmail.com>
2026-06-02 08:08:42 +0700

    thêm API

--- src/pages/services/getAPI.js
+++ src/pages/services/getAPI.js
@@ -2,6 +2,9 @@ const API_LIST = [
     "https://tuyen-sinh-26-27-khoi-6-be.onrender.com",
     "https://tuyen-sinh-26-27-khoi-6-be-3.onrender.com",
     "https://tuyen-sinh-26-27-khoi-6-be-2.onrender.com",
+    "https://tuyen-sinh-26-27-khoi-6-be.vercel.app",
+    "https://tuyen-sinh-26-27-khoi-6-be-wivf.vercel.app",
+    "https://tuyen-sinh-26-27-khoi-6-be-jgeg.vercel.app",
 ];
 
 const getRandomApi = () => {
